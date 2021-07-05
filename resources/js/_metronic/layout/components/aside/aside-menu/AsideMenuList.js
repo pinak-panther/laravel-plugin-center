4ed15c5025091f5dad37076dd2091eff4e739869
Pinak Patel <pinak.panther2021@gmail.com>
2021-07-05 18:52:04 +0530

    Added css code for submenu

--- resources/js/_metronic/layout/components/aside/aside-menu/AsideMenuList.js
+++ resources/js/_metronic/layout/components/aside/aside-menu/AsideMenuList.js
@@ -5,163 +5,191 @@ import {useLocation} from "react-router";
 import {NavLink} from "react-router-dom";
 import SVG from "react-inlinesvg";
 import {toAbsoluteUrl, checkIsActive} from "../../../../_helpers";
+import {makeStyles} from "@material-ui/core/styles";
 
 export function AsideMenuList({layoutProps}) {
     const location = useLocation();
-    const getMenuItemActive = (url, hasSubmenu = false) => {
-        return checkIsActive(location, url)
-            ? ` ${!hasSubmenu &&
-            "menu-item-active"} menu-item-open menu-item-not-hightlighted`
-            : "";
-    };
+
+    const useStyles = makeStyles(theme => ({
+       menu:{
+           fontSize:'1.3rem',
+           color:'white',
+           listStyleType:'none'
+       },
+       submenu:{
+           fontSize:'0.9rem',
+           color:'white',
+           textDecoration:'none'
+       }
+    }));
+    const classes = useStyles();
+    // return (
+    //     <>
+    //         {/* begin::Menu Nav */}
+    //         <ul className={`menu-nav ${layoutProps.ulClasses}`}>
+    //             {/* Material-UI */}
+    //             {/*begin::1 Level*/}
+    //             <li
+    //                 className={`menu-item menu-item-submenu ${getMenuItemActive(
+    //                     "/google-material",
+    //                     true
+    //                 )}`}
+    //                 aria-haspopup="true"
+    //                 data-menu-toggle="hover"
+    //             >
+    //                 <NavLink className="menu-link menu-toggle" to="/google-material">
+    //                     <span className="svg-icon menu-icon">
+    //                       <SVG src={toAbsoluteUrl("/media/svg/icons/Design/Cap-2.svg")} />
+    //                     </span>
+    //                     <span className="menu-text">Plugin Center</span>
+    //                     <i className="menu-arrow" />
+    //                 </NavLink>
+    //                 <div className="menu-submenu ">
+    //                     <i className="menu-arrow" />
+    //                     <ul className="menu-subnav">
+    //                         <li className="menu-item  menu-item-parent" aria-haspopup="true">
+    //             <span className="menu-link">
+    //               <span className="menu-text">Material UI</span>
+    //             </span>
+    //                         </li>
+    //
+    //                         {/* Inputs */}
+    //                         {/*begin::2 Level*/}
+    //                         <li
+    //                             className={`menu-item menu-item-submenu ${getMenuItemActive(
+    //                                 "/application-list",
+    //                                 true
+    //                             )}`}
+    //                             aria-haspopup="true"
+    //                             data-menu-toggle="hover"
+    //                         >
+    //                             <NavLink
+    //                                 className="menu-link menu-toggle"
+    //                                 to="/application-list"
+    //                             >
+    //                                 <i className="menu-bullet menu-bullet-dot">
+    //                                     <span />
+    //                                 </i>
+    //                                 <span className="menu-text">Applications</span>
+    //                                 <i className="menu-arrow" />
+    //                             </NavLink>
+    //                             <div className="menu-submenu ">
+    //                                 <i className="menu-arrow" />
+    //                                 <ul className="menu-subnav">
+    //                                     {/*begin::3 Level*/}
+    //                                     <li
+    //                                         className={`menu-item  ${getMenuItemActive(
+    //                                             "/application-list"
+    //                                         )}`}
+    //                                         aria-haspopup="true"
+    //                                     >
+    //                                         <NavLink
+    //                                             className="menu-link"
+    //                                             to="/application-list"
+    //                                         >
+    //                                             <i className="menu-bullet menu-bullet-dot">
+    //                                                 <span />
+    //                                             </i>
+    //                                             <span className="menu-text">Application List</span>
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     {/*end::3 Level*/}
+    //
+    //                                     {/*begin::3 Level*/}
+    //                                     <li
+    //                                         className={`menu-item ${getMenuItemActive(
+    //                                             "/application-add"
+    //                                         )}`}
+    //                                         aria-haspopup="true"
+    //                                     >
+    //                                         <NavLink
+    //                                             className="menu-link"
+    //                                             to="/application-add"
+    //                                         >
+    //                                             <i className="menu-bullet menu-bullet-dot">
+    //                                                 <span />
+    //                                             </i>
+    //                                             <span className="menu-text">Application Add</span>
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     {/*end::3 Level*/}
+    //
+    //                                 </ul>
+    //                             </div>
+    //                         </li>
+    //                         {/*end::2 Level*/}
+    //
+    //                         {/* Stores */}
+    //                         {/*begin::2 Level*/}
+    //                         <li
+    //                             className={`menu-item menu-item-submenu ${getMenuItemActive(
+    //                                 "/store-list",
+    //                                 true
+    //                             )}`}
+    //                             aria-haspopup="true"
+    //                             data-menu-toggle="hover"
+    //                         >
+    //                             <NavLink
+    //                                 className="menu-link menu-toggle"
+    //                                 to="/store-list"
+    //                             >
+    //                                 <i className="menu-bullet menu-bullet-dot">
+    //                                     <span />
+    //                                 </i>
+    //                                 <span className="menu-text">Stores</span>
+    //                                 <i className="menu-arrow" />
+    //                             </NavLink>
+    //                             <div className="menu-submenu">
+    //                                 <i className="menu-arrow" />
+    //                                 <ul className="menu-subnav">
+    //                                     {/*begin::3 Level*/}
+    //                                     <li
+    //                                         className={`menu-item ${getMenuItemActive(
+    //                                             "/store-list"
+    //                                         )}`}
+    //                                         aria-haspopup="true"
+    //                                     >
+    //                                         <NavLink
+    //                                             className="menu-link"
+    //                                             to="/store-list"
+    //                                         >
+    //                                             <i className="menu-bullet menu-bullet-dot">
+    //                                                 <span />
+    //                                             </i>
+    //                                             <span className="menu-text">Store List</span>
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     {/*end::3 Level*/}
+    //                                 </ul>
+    //                             </div>
+    //                         </li>
+    //                         {/*end::2 Level*/}
+    //                     </ul>
+    //                 </div>
+    //             </li>
+    //             {/*end::1 Level*/}
+    //         </ul>
+    //         {/* end::Menu Nav */}
+    //     </>
+    // );
 
     return (
         <>
-            {/* begin::Menu Nav */}
-            <ul className={`menu-nav ${layoutProps.ulClasses}`}>
-                {/* Material-UI */}
-                {/*begin::1 Level*/}
-                <li
-                    className={`menu-item menu-item-submenu ${getMenuItemActive(
-                        "/google-material",
-                        true
-                    )}`}
-                    aria-haspopup="true"
-                    data-menu-toggle="hover"
-                >
-                    <NavLink className="menu-link menu-toggle" to="/google-material">
-            <span className="svg-icon menu-icon">
-              <SVG src={toAbsoluteUrl("/media/svg/icons/Design/Cap-2.svg")} />
-            </span>
-                        <span className="menu-text">Plugin Center</span>
-                        <i className="menu-arrow" />
-                    </NavLink>
-                    <div className="menu-submenu ">
-                        <i className="menu-arrow" />
-                        <ul className="menu-subnav">
-                            <li className="menu-item  menu-item-parent" aria-haspopup="true">
-                <span className="menu-link">
-                  <span className="menu-text">Material UI</span>
-                </span>
-                            </li>
-
-                            {/* Inputs */}
-                            {/*begin::2 Level*/}
-                            <li
-                                className={`menu-item menu-item-submenu ${getMenuItemActive(
-                                    "/application-list",
-                                    true
-                                )}`}
-                                aria-haspopup="true"
-                                data-menu-toggle="hover"
-                            >
-                                <NavLink
-                                    className="menu-link menu-toggle"
-                                    to="/application-list"
-                                >
-                                    <i className="menu-bullet menu-bullet-dot">
-                                        <span />
-                                    </i>
-                                    <span className="menu-text">Applications</span>
-                                    <i className="menu-arrow" />
-                                </NavLink>
-                                <div className="menu-submenu ">
-                                    <i className="menu-arrow" />
-                                    <ul className="menu-subnav">
-                                        {/*begin::3 Level*/}
-                                        <li
-                                            className={`menu-item  ${getMenuItemActive(
-                                                "/application-list"
-                                            )}`}
-                                            aria-haspopup="true"
-                                        >
-                                            <NavLink
-                                                className="menu-link"
-                                                to="/application-list"
-                                            >
-                                                <i className="menu-bullet menu-bullet-dot">
-                                                    <span />
-                                                </i>
-                                                <span className="menu-text">Application List</span>
-                                            </NavLink>
-                                        </li>
-                                        {/*end::3 Level*/}
-
-                                        {/*begin::3 Level*/}
-                                        <li
-                                            className={`menu-item ${getMenuItemActive(
-                                                "/application-add"
-                                            )}`}
-                                            aria-haspopup="true"
-                                        >
-                                            <NavLink
-                                                className="menu-link"
-                                                to="/application-add"
-                                            >
-                                                <i className="menu-bullet menu-bullet-dot">
-                                                    <span />
-                                                </i>
-                                                <span className="menu-text">Application Add</span>
-                                            </NavLink>
-                                        </li>
-                                        {/*end::3 Level*/}
-
-                                    </ul>
-                                </div>
-                            </li>
-                            {/*end::2 Level*/}
-
-                            {/* Stores */}
-                            {/*begin::2 Level*/}
-                            <li
-                                className={`menu-item menu-item-submenu ${getMenuItemActive(
-                                    "/store-list",
-                                    true
-                                )}`}
-                                aria-haspopup="true"
-                                data-menu-toggle="hover"
-                            >
-                                <NavLink
-                                    className="menu-link menu-toggle"
-                                    to="/store-list"
-                                >
-                                    <i className="menu-bullet menu-bullet-dot">
-                                        <span />
-                                    </i>
-                                    <span className="menu-text">Stores</span>
-                                    <i className="menu-arrow" />
-                                </NavLink>
-                                <div className="menu-submenu">
-                                    <i className="menu-arrow" />
-                                    <ul className="menu-subnav">
-                                        {/*begin::3 Level*/}
-                                        <li
-                                            className={`menu-item ${getMenuItemActive(
-                                                "/store-list"
-                                            )}`}
-                                            aria-haspopup="true"
-                                        >
-                                            <NavLink
-                                                className="menu-link"
-                                                to="/store-list"
-                                            >
-                                                <i className="menu-bullet menu-bullet-dot">
-                                                    <span />
-                                                </i>
-                                                <span className="menu-text">Store List</span>
-                                            </NavLink>
-                                        </li>
-                                        {/*end::3 Level*/}
-                                    </ul>
-                                </div>
-                            </li>
-                            {/*end::2 Level*/}
-                        </ul>
-                    </div>
-                </li>
-                {/*end::1 Level*/}
-            </ul>
-            {/* end::Menu Nav */}
+          <ul style={{marginTop:'80px'}}>
+              <li className={classes.menu}>Application</li>
+              <div>
+                  <ul>
+                      <li className={classes.submenu}>List Application</li>
+                      <li className={classes.submenu}>Add Application</li>
+                  </ul>
+              </div>
+              <li className={classes.menu}>Store</li>
+              <div>
+                  <ul>
+                      <li className={classes.submenu}>List Store</li>
+                  </ul>
+              </div>
+          </ul>
         </>
     );
 }
